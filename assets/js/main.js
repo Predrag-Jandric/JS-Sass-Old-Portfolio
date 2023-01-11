b989ecfb762fc95638b3bb63de059a88f3ac3d44
predrag jandric <predrag.jandric.bg@gmail.com>
2023-01-11 23:27:56 +0100

    CREATED: initial project structure along with mixins, variables and nav section in directory partials. quick project remake after i messed up something while setting up new user in git and changing my user name in git hub

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// Open/close mobile menu
-
-const navBtn = document.getElementById("hamburger");
-const navList = document.getElementById('nav-list');
-
-navBtn.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
