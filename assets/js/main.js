585b9871cdf8e87ca2ec64dd500669a1f9e7ee42
predrag jandric <predrag.jandric.bg@gmail.com>
2023-01-13 00:00:07 +0100

    UPDATE: some minor updates mainly in font selection and added text appear effect in header section through JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,23 @@ const navList = document.getElementById('nav-list');
 navBtn.addEventListener('click', () => {
     navList.classList.toggle('show');
 });
+
+
+// heading text appear
+
+const textElement = document.getElementById('write-heading')
+const text = `Let's make the web interesting`
+
+let idx = 3
+
+writeHeading()
+
+function writeHeading() {
+    textElement.innerText = text.slice(0, idx)
+
+    idx++
+
+    setTimeout(writeHeading, 20)
+}
+
+// about secion appear effect
