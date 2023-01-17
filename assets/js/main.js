6771f1146ad9bafceacb84e4928bcf96c73ff996
predrag jandric <predrag.jandric.bg@gmail.com>
2023-01-17 01:36:21 +0100

    UPDATED: header background image changed, added earth and moon images and added parallax effect on mouse move for them, and a few minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,28 @@ navBtn.addEventListener('click', () => {
 });
 
 
-// Header section appear effect
+// Header section appear effect, immediately upon entering site
 var reveals = document.querySelectorAll('.revealHeaderJS');
   reveals.forEach(function(e){
     e.classList.add('active');
-});
+  });
 
 
-// About section and Footer section appear effect
+// Header mouse move parallax effect
+document.addEventListener("mousemove", parallax);
+  function parallax(e){
+    document.querySelectorAll(".mouseMoveParallaxJS").forEach(function(move){
+
+    var moving_value = move.getAttribute("data-value");
+    var x = (e.clientX * moving_value) / 50;
+    var y = (e.clientY * moving_value) / 50;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    }
+  )}
+
+
+// About section and Footer section scroll appear effect
 window.addEventListener('scroll', revealAbout);
 
   function revealAbout(){
@@ -28,14 +42,14 @@ window.addEventListener('scroll', revealAbout);
       if(revealtop < windowheight - revealpoint){
         reveals[i].classList.add('active');
       }
-      // uncomment this else statement to make elements' animations show multiple times
+      // uncomment else statement to make elements' animations show multiple times
       // else{
       //   reveals[i].classList.remove('active');
       // }
     }}
 
 
-  // Skills section parallax effect
+// Skills section scroll parallax effect
 const skillsSection = document.getElementById('parallaxJS');
 
   window.addEventListener('scroll', function() {
